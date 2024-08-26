c0571ba65d1e6868635b905fb4c80667bffd1cfd
LuthMC <luthfi366822@gmail.com>
2024-08-27 01:11:06 +0700

    Add /unnick

--- src/Luthfi/SimpleNick/commands/NickCommand.php
+++ src/Luthfi/SimpleNick/commands/NickCommand.php
@@ -14,35 +14,43 @@ class NickCommand extends Command {
     private bool $defaultPermission;
 
     public function __construct(bool $defaultPermission) {
-        parent::__construct("nick", "Change your nickname", "/nick <nickname>", []);
+        parent::__construct("nick", "Change your nickname", "/nick <nickname>");
         $this->defaultPermission = $defaultPermission;
 
-        if ($defaultPermission) {
-            $this->setPermission(null);
-        } else {
-            $this->setPermission("sn.use");
-        }
+        $this->setPermission($defaultPermission ? null : "sn.use");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$this->defaultPermission && !$this->testPermission($sender)) {
-            $sender->sendMessage(TextFormat::RED . "You do not have permission to use this command.");
-            return false;
-        }
-
         if (!$sender instanceof Player) {
             $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
             return false;
         }
 
-        if (count($args) < 1) {
-            $sender->sendMessage(TextFormat::RED . "Usage: /nick <nickname>");
+        if (!$this->defaultPermission && !$this->testPermission($sender)) {
+            $sender->sendMessage(TextFormat::RED . "You do not have permission to use this command.");
             return false;
         }
 
-        $nickname = implode(" ", $args);
-        $sender->setDisplayName($nickname);
-        $sender->sendMessage(TextFormat::GREEN . "Your nickname has been changed to " . TextFormat::YELLOW . $nickname);
+        if (strtolower($commandLabel) === "nick") {
+            if (empty($args)) {
+                $sender->sendMessage(TextFormat::RED . "Usage: /nick <nickname>");
+                return false;
+            }
+
+            $nickname = implode(" ", $args);
+
+            if (strlen($nickname) > 16) {
+                $sender->sendMessage(TextFormat::RED . "Nickname is too long. Maximum length is 16 characters.");
+                return false;
+            }
+
+            $sender->setDisplayName($nickname);
+            $sender->sendMessage(TextFormat::GREEN . "Your nickname has been changed to " . TextFormat::YELLOW . $nickname);
+
+        } elseif (strtolower($commandLabel) === "unnick") {
+            $sender->setDisplayName($sender->getName());
+            $sender->sendMessage(TextFormat::GREEN . "Your nickname has been reset to your default name: " . TextFormat::YELLOW . $sender->getName());
+        }
 
         return true;
     }
