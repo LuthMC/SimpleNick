406ee7ed1f7573947dbc37d5517cf04a24ac9780
LuthMC <luthfi366822@gmail.com>
2024-08-27 02:04:07 +0700

    Update Main.php

--- src/Luthfi/SimpleNick/Main.php
+++ src/Luthfi/SimpleNick/Main.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace Luthfi\SimpleNick;
 
 use pocketmine\plugin\PluginBase;
+use Luthfi\SimpleNick\commands\NickCommand;
+use Luthfi\SimpleNick\commands\UnnickCommand;
 
 class Main extends PluginBase {
 
+    private bool $defaultPermission;
+    private string $defaultNickname;
     private int $maxNicknameLength;
     private string $allowedCharacters;
     private array $messages;
@@ -15,37 +19,19 @@ class Main extends PluginBase {
     public function onEnable(): void {
         $this->saveDefaultConfig();
         $config = $this->getConfig();
-
+        $this->defaultPermission = $config->get("default", false);
+        $this->defaultNickname = $config->get("default_nickname", "Player");
         $this->maxNicknameLength = $config->get("max_nickname_length", 16);
-        $this->allowedCharacters = $config->get("allowed_characters", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        $this->allowedCharacters = $config->get("allowed_characters", "a-zA-Z0-9_");
         $this->messages = $config->get("messages", []);
 
-        $this->getServer()->getCommandMap()->register("nick", new \Luthfi\SimpleNick\commands\NickCommand(
-            $this->maxNicknameLength,
-            $this->allowedCharacters,
-            $this->messages
-        ));
-
-        $this->getServer()->getCommandMap()->register("unnick", new \Luthfi\SimpleNick\commands\UnnickCommand(
-            $this->messages
-        ));
+        $this->getServer()->getCommandMap()->register("nick", new NickCommand($this, $this->maxNicknameLength, $this->allowedCharacters, $this->messages));
+        $this->getServer()->getCommandMap()->register("unnick", new UnnickCommand($this, $this->messages));
 
         $this->getLogger()->info("SimpleNick Enabled");
     }
 
     public function onDisable(): void {
         $this->getLogger()->info("SimpleNick Disabled");
-    }
-
-    public function getMaxNicknameLength(): int {
-        return $this->maxNicknameLength;
-    }
-
-    public function getAllowedCharacters(): string {
-        return $this->allowedCharacters;
-    }
-
-    public function getMessages(): array {
-        return $this->messages;
     }
 }
